ce3c239e0b38dbfeda9a4b0db4d1c719b2dbc9b8
JAMAN255 <manjakub@seznam.cz>
2026-05-28 13:59:34 +0200

    User issues

--- myapp/homepage/admin.py
+++ myapp/homepage/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name', 'user_type')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'name', 'user_type', 'is_staff', 'is_superuser')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'user_type', 'date_joined')
+    search_fields = ('email', 'name')
+    ordering = ('email',)
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 
